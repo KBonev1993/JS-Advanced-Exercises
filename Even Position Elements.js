15ed4e53a955f02e11906e90ec238eb039338d75
Kristiyan Bonev <85802301+KBonev1993@users.noreply.github.com>
2022-07-15 16:49:46 +0300

    Create Even Position Elements.js

---
+++ Even Position Elements.js
@@ -0,0 +1,8 @@
+function positions(input){
+    let result = '';
+    for (let index = 0; index < input.length; index += 2) {
+            result += input[index];
+            result += " "
+        }
+        console.log(result); 
+    }
