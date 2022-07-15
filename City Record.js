36ff64334d0017d1601bbeda4bddd403a69cda2b
Kristiyan Bonev <85802301+KBonev1993@users.noreply.github.com>
2022-07-15 17:01:39 +0300

    Create City Record.js

---
+++ City Record.js
@@ -0,0 +1,8 @@
+function cityRecord(town, population, treasury){
+    let objectProperties = {
+        name: town,
+        population: population,
+        treasury: treasury
+    }
+    return(objectProperties);
+}
