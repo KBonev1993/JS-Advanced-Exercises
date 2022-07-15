fc33ea96b7afd4731ee8f57e7c6fb1a8aa861b31
Kristiyan Bonev <85802301+KBonev1993@users.noreply.github.com>
2022-07-15 17:07:09 +0300

    Create City Taxes.js

---
+++ City Taxes.js
@@ -0,0 +1,18 @@
+function cityTaxes(name,population,treasury){
+    let cityObject = {
+        name,
+        population,
+        treasury,
+        taxRate: 10,
+        collectTaxes(){
+            this.treasury += Math.floor(this.population * this.taxRate);
+        },
+        applyGrowth(percentage){
+            this.population += Math.floor(this.population * percentage / 100);
+        },
+        applyRecession(percentage){
+            this.treasury -= Math.floor(this.treasury * percentage / 100);
+        }
+    }
+    return cityObject;
+}
