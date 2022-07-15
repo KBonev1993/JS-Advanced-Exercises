f91b48fd662a6b217fec990950b8d0aa59d54170
Kristiyan Bonev <85802301+KBonev1993@users.noreply.github.com>
2022-07-15 17:22:30 +0300

    Create Person.js

---
+++ Person.js
@@ -0,0 +1,13 @@
+class Person{
+    constructor(firstName, lastName, age, email){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.email = email;
+    }
+
+    toString(){
+        let personalInfo = `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
+        return personalInfo;
+    }
+}
