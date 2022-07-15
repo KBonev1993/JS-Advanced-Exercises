19d641792ef6407a1d71817605c3a970f15ce147
Kristiyan Bonev <85802301+KBonev1993@users.noreply.github.com>
2022-07-15 17:12:04 +0300

    Create DOM (Show More).js

---
+++ DOM (Show More).js
@@ -0,0 +1,6 @@
+function showText() {
+    let buttonReadMore = document.getElementById("more").value;
+    document.getElementById("text").style = "display: inline"
+    document.getElementById("more").style = "display: none"
+
+}
